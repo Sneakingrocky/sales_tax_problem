a62a741379f84b6052af77dc14e98f7374d833e6
Erica <garciadesign.me@gmail.com>
2014-09-11 14:39:15 -0500

    Able to get import duty tax when given a price and a set import tax rate of 5%

--- lib/receipt_generator.rb
+++ lib/receipt_generator.rb
@@ -1,7 +1,13 @@
 class ReceiptGenerator
 
-  def get_sales_tax(price)
+  def get_basic_sales_tax(price)
     sales_tax_rate = 0.10
     price * sales_tax_rate
   end
+
+  def get_import_duty_tax(price)
+    import_tax_rate = 0.05
+    price * import_tax_rate  
+  end
+    
 end
--- spec/receipt_generator_spec.rb
+++ spec/receipt_generator_spec.rb
@@ -3,18 +3,31 @@ require './lib/receipt_generator.rb'
 
 describe ReceiptGenerator do
 
-  describe "get_sales_tax" do
-    it "returns a tax of 1.5 when passed a price of 15" do
-      expect(ReceiptGenerator.new.get_sales_tax(15)).to eq(1.5)
+  describe "get_basic_sales_tax" do
+    it "returns a basic tax of 1.5 when passed a price of 15" do
+      expect(ReceiptGenerator.new.get_basic_sales_tax(15)).to eq(1.5)
     end
 
-    it "returns a tax of 2 when passed a price of 20" do
-      expect(ReceiptGenerator.new.get_sales_tax(20)).to eq(2)
+    it "returns a basic tax of 2 when passed a price of 20" do
+      expect(ReceiptGenerator.new.get_basic_sales_tax(20)).to eq(2)
     end
 
-    it "returns a tax of 0.4 when passed a price of 4" do
-      expect(ReceiptGenerator.new.get_sales_tax(4)).to eq(0.4)
+    it "returns a basic tax of 0.4 when passed a price of 4" do
+      expect(ReceiptGenerator.new.get_basic_sales_tax(4)).to eq(0.4)
     end
   end
 
+  describe "get_import_duty_tax" do
+    it "returns an import duty tax of .75 when passed a price of 15" do
+      expect(ReceiptGenerator.new.get_import_duty_tax(15)).to eq(0.75)
+    end  
+
+    it "returns an import duty tax of 1 when passed a price of 20" do
+      expect(ReceiptGenerator.new.get_import_duty_tax(20)).to eq(1)
+    end  
+
+    it "returns an import duty tax of .75 when passed a price of 15" do
+      expect(ReceiptGenerator.new.get_import_duty_tax(4)).to eq(0.2)
+    end  
+  end  
 end
