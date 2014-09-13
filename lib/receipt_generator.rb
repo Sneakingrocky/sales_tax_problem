fadeb3905e5fba4d4fe381f563d3bf7cc7a0542c
Erica <garciadesign.me@gmail.com>
2014-09-12 23:06:40 -0500

    created method to combine basic and import tax, created method to round tax to the nearest nickel

--- lib/receipt_generator.rb
+++ lib/receipt_generator.rb
@@ -1,13 +1,28 @@
+require 'bigdecimal'
+
 class ReceiptGenerator
+  SALES_TAX_RATE = 0.1
+  IMPORT_TAX_RATE = 0.05
+  attr_reader :price_cents
 
-  def get_basic_sales_tax(price)
-    sales_tax_rate = 0.10
-    price * sales_tax_rate
+  def initialize(price_cents)
+    @price_cents = price_cents
   end
 
-  def get_import_duty_tax(price)
-    import_tax_rate = 0.05
-    price * import_tax_rate  
+  def basic_sales_tax_cents
+    price_cents * SALES_TAX_RATE
   end
-    
+
+  def import_duty_tax_cents
+    price_cents * IMPORT_TAX_RATE
+  end
+
+  def combined_tax_cents
+    tax_total = basic_sales_tax_cents + import_duty_tax_cents
+    round_to_next_nickel(tax_total)
+  end
+  
+  def round_to_next_nickel(amount_cents)
+    amount = (amount_cents / 5.0).ceil * 5.0
+  end
 end
