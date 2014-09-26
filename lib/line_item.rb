91e5ee8992112f7e995a5e8a63f2398f63261c79
Erica <garciadesign.me@gmail.com>
2014-09-26 10:15:49 -0500

    Added methods to LineItem class to handle basic, import and total tax of a line item. Also added a method ot handle calculating the total(tax + price)

--- lib/line_item.rb
+++ lib/line_item.rb
@@ -3,5 +3,36 @@ class LineItem
 
   def imported?
     imported
-  end   
+  end  
+
+  def sales_tax_in_cents
+    tax_calculator = TaxCalculator.new(price_in_cents)
+    
+    if !TaxExempt.item_exempt?(product)
+      tax_calculator.basic_sales_tax_in_cents
+    else
+      0
+    end   
+  end 
+
+  def import_tax_in_cents 
+    tax_calculator = TaxCalculator.new(price_in_cents)
+
+    if imported?
+      tax_calculator.import_duty_tax_in_cents
+    else
+      0
+    end    
+  end  
+
+  def total_tax_in_cents
+    tax_calculator = TaxCalculator.new(price_in_cents)
+    
+    total_tax_in_cents = sales_tax_in_cents + import_tax_in_cents
+    tax_calculator.round_to_next_nickel(total_tax_in_cents)
+  end
+
+  def total_in_cents
+    price_in_cents + total_tax_in_cents
+  end  
 end  
---
+++ spec/line_item_spec.rb
@@ -0,0 +1,105 @@
+require './lib/line_item'
+require './lib/tax_calculator'
+require './lib/tax_exempt'
+
+describe LineItem do
+  let(:line_item) do
+    LineItem.new.tap do |line_item|
+      line_item.quantity = quantity
+      line_item.imported = imported
+      line_item.product = product
+      line_item.price_in_cents = price_in_cents
+    end
+  end
+  let(:imported) {false}
+  let(:product) { "music CD" }
+  let(:quantity) { 1 }
+  let(:price_in_cents) { 10_00 }
+
+  describe "sales_tax_in_cents" do
+    
+    context "non exempt item" do
+      let(:imported) {false}
+      let(:product) { "music CD" }
+      let(:quantity) { 1 }
+      let(:price_in_cents) { 10_00 }
+
+      it "calculates tax in cents of a line item" do
+        expect(line_item.sales_tax_in_cents).to eq(1_00)
+      end
+    end  
+
+    context "exempt item" do
+      let(:imported) {false}
+      let(:product) { "packet of headache pills" }
+      let(:quantity) { 1 }
+      let(:price_in_cents) { 9_75}
+
+      it "returns 0 tax for an exempt item" do
+        expect(line_item.sales_tax_in_cents).to eq(0)
+      end
+    end
+  end
+
+  describe "import_tax_in_cents" do
+
+    context "imported item" do
+      let(:imported) {true}
+      let(:product) { "box of chocolates" }
+      let(:quantity) { 1 }
+      let(:price_in_cents) { 10_00 }
+
+      it "calculates tax in cents of an imported line item" do
+        expect(line_item.import_tax_in_cents).to eq(50)
+      end
+    end
+
+    context "non imported item" do
+      let(:imported) {false}
+      let(:product) { "packet of headache pills" }
+      let(:quantity) { 1 }
+      let(:price_in_cents) { 9_75 }
+
+      it "returns 0 tax for an item that is not imported " do
+        expect(line_item.import_tax_in_cents).to eq(0)
+      end
+    end
+  end
+
+
+  describe "total tax" do
+
+    context "imported item with basic sales tax" do
+
+      it "adds the two taxes together" do
+        allow(line_item).to receive(:sales_tax_in_cents).and_return(5_00)
+        allow(line_item).to receive(:import_tax_in_cents).and_return(1_00)
+        expect(line_item.total_tax_in_cents).to eq(6_00)
+      end
+      
+      it "adds round the total tax to the nearest nickel" do
+        allow(line_item).to receive(:sales_tax_in_cents).and_return(5_63)
+        allow(line_item).to receive(:import_tax_in_cents).and_return(1_89)
+        expect(line_item.total_tax_in_cents).to eq(7_55)
+      end
+    end
+  end
+
+  describe "total_in_cents" do
+
+    context "calculates the total of the line item" do
+
+      it "adds the two taxes together" do
+        allow(line_item).to receive(:total_tax_in_cents).and_return(1_00)
+        allow(line_item).to receive(:price_in_cents).and_return(32_19)
+
+        expect(line_item.total_in_cents).to eq(33_19)
+      end
+    end
+  end
+
+
+
+end  
+
+
